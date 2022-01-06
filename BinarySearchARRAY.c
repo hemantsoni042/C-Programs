1bb9c6219e781d6b2294ab7586e3b87e31df4b9c
hemantsoni042 <20egjcs042@gitjaipur.com>
2022-01-06 11:27:03 +0530

    commit

---
+++ BinarySearchARRAY.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void binarysearch(int ar[], int value, int lb, int ub)
+{
+    int index = (lb + ub) / 2;
+    if (ar[index] == value)
+        printf("Given %d value is find at %d position !!", value, index+1);
+    else if (ar[index] > value)
+        binarysearch(ar, value, 0, index - 1);
+    else if (ar[index] < value)
+        binarysearch(ar, value, index + 1, ub);
+    else
+        printf("Given %d value is not in the given array !!", value);
+}
+
+int main()
+{
+    int n, i, value;
+    printf("Enter number of elements :- ");
+    scanf("%d", &n);
+    int ar[n];
+    printf("Enter %d elements ' elements must be in sorted form ' :- ", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &ar[i]);
+    }
+
+    printf("Which number you want to search in the array :- ");
+    scanf("%d", &value);
+
+    binarysearch(ar, value, 0, n - 1);
+
+    return 0;
+}
