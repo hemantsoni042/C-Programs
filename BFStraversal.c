6bf63ce5fabc20efc4d9f05e2e39abfc8e30f6d3
hemantsoni042 <20egjcs042@gitjaipur.com>
2022-03-13 21:02:47 +0530

    commit

--- BFStraversal.c
+++ BFStraversal.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int a[20][20],q[20], visited[20], n, i, j, f = 0, r = -1;
+int a[20][20],q[20]={0}, visited[20]={0}, n, i, j, f = 0, r = -1;
 
 void bfs(int v)
 {
     for (i = 0; i < n; i++)
-        if (a[v][1] && !visited[i])
+        if (a[v][i] && !visited[i])
             q[++r] = i;
     if (f <= r)
     {
@@ -16,17 +16,9 @@ void bfs(int v)
 
 int main()
 {
-
-
     printf("\n Enter the number of vertices :- ");
 
     scanf("%d", &n);
-
-    for (i = 0; i < n; i++)
-    {
-        q[i] = 0; 
-        visited[i] = 0;
-    }
 
     printf("\n Enter graph data in matrix form: \n");
     for (i = 0; i < n; i++)
@@ -45,4 +37,3 @@ int main()
     return 0;
     
 }
-
--- DFStraversal.c
+++ DFStraversal.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
-int ar[10][10], visited[10], n;
+int ar[10][10], visited[10] = {0}; // intialize visited stack as 0 
+int n;
 
-void dfs(int i)
+void dfs(int i,int j)
 {
-    int j;
+    
     printf("\n%d", i);
     visited[i] = 1;
-    for (j = 0; j< n; j++)
+    for (j=0; j<n; j++)   
     {
         if (!visited[j] && ar[i][j] == 1)
-            dfs(j);
+            dfs(j,j);
     }
 }
 int main()
@@ -18,14 +19,12 @@ int main()
     int i, j;
     printf("Enter number of vertices :- ");
     scanf("%d",&n);
-    printf("Enter adjecency matrix of the graph :- ");
+    printf("Enter adjecency matrix of the graph :- \n");
     for (i = 0; i < n; i++)
         for (j = 0; j < n; j++)
             scanf("%d", &ar[i][j]);
 
-    for (i = 0; i < n; i++)
-        visited[i] = 0;
-    dfs(0);
+    dfs(0,0);
 
     return 0;
 }
